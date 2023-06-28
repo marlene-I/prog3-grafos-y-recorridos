2033a1ddd15a042b9a20089d709d95a3d5d3fc8b
Marlene Carli <maarlen.car@hotmail.com>
2023-06-28 10:50:33 -0300

    Cambio de nombre de carpeta

---
+++ TPE/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		String path = "PATH/AL/ARCHIVO";
+		CSVReader reader = new CSVReader(path);
+		reader.read();
+		
+	}
+
+}
+ 
